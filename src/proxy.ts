f26259706164df50ec59c0725f2d3c0bc27bee66
niteshogrey <ogreynitesh1102@gmail.com>
2026-01-22 17:02:11 +0530

    update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { decodeJwt } from "../lib/edgeAuth";
+import { decodeJwt } from "./lib/edgeAuth";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
