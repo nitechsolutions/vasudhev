5ab49dbe1ab5e811825b69e3c20b7e827bacd20d
niteshogrey <ogreynitesh1102@gmail.com>
2026-01-27 20:29:23 +0530

    fixed user/id bug

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,14 +3,21 @@ import User from "@/models/User";
 import { getUserFromCookies } from "@/lib/getUserFromRequest";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req: NextRequest,
-  { params }: { params: Promise<{ slug: string }> }) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> } // ✅ id, not slug
+) {
   await connectDB();
-  const admin = await getUserFromCookies(req);
-const {id} = await params
 
-  if (!admin || admin.role !== "admin")
-    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  const admin = await getUserFromCookies(req);
+  const { id } = await params; // ✅ matches folder name
+
+  if (!admin || admin.role !== "admin") {
+    return NextResponse.json(
+      { error: "Forbidden" },
+      { status: 403 }
+    );
+  }
 
   const { role } = await req.json();
 
