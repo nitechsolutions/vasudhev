62f9d0034159a1014fbb5783bb0376c5e4defe93
niteshogrey <ogreynitesh1102@gmail.com>
2026-05-07 01:37:32 +0530

    fixed indexing

--- src/app/components/home/CategorySection.tsx
+++ src/app/components/home/CategorySection.tsx
@@ -34,7 +34,7 @@ export default function CategorySection({
       {posts.map((post) => {
         return (
           <Link key={post.slug} href={`/${post.category}/${post.slug}`} className="flex gap-3 mb-3">
-            <img  src={post.image} className=" h-16 object-cover rounded" />
+            <img  src={post.image} alt={post.slug} className=" h-16 object-cover rounded" />
             <p className="hover:text-red-600 active:text-red-600 text-gray-700 transition font-semibold">
               {post.title}
             </p>
