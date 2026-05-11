f15b4fa74116505ae515edf46644ddb584b5552b
niteshogrey <ogreynitesh1102@gmail.com>
2026-05-11 21:27:36 +0530

    update ssr client

--- src/lib/service/auth.client.ts
+++ src/lib/service/auth.client.ts
@@ -1,4 +1,10 @@
-import { supabase } from "../supabase/client";
+import { createBrowserClient } from "@supabase/ssr";
+
+ const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
+
 
 /* -------- LOGIN -------- */
 export async function loginUser(email: string, password: string) {
